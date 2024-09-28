b22efbe398b13fdb2805e49d14abcd14ac9a3276
RecurrciveItu <149315193+RecurrciveItu@users.noreply.github.com>
2024-09-28 20:21:38 +0530

    craeting ui from bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Sidebar />
+      <div className="content">
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
