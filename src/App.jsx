3b650908d6e4bb202164b9471387b3298574f9ee
RecurrciveItu <149315193+RecurrciveItu@users.noreply.github.com>
2024-09-29 10:10:08 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
+import "./App.css";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
-import "./App.css";
-
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import PostListProvider from "./store/post-lis-store";
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
   return (
-    <div className="app-container">
-      <Sidebar />
-      <div className="content">
-        <Header />
-        <Footer />
+    <PostListProvider>
+      <div className="app-container">
+        <Sidebar setSelectedTab={setSelectedTab} selectedTab={selectedTab} />
+        <div className="content">
+          <Header />
+          {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+          <Footer />
+        </div>
       </div>
-    </div>
+    </PostListProvider>
   );
 }
 
