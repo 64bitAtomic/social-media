e2b5316e8fbf6f7cfb087c1e70ae0f744228b9ef
RecurrciveItu <149315193+RecurrciveItu@users.noreply.github.com>
2024-09-29 19:53:11 +0530

    craete post

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,44 +1,98 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
+import { PostList } from "../store/post-lis-store";
 
 const CreatePost = () => {
+  const { addPost } = useContext(PostList);
+  const userIdElement = useRef();
+  const postTitleElement = useRef();
+  const postBodyElement = useRef();
+  const reactionsElement = useRef();
+  const tagsElement = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userId = userIdElement.current.value;
+    const postTitle = postTitleElement.current.value;
+    const postBody = postBodyElement.current.value;
+    const reactions = reactionsElement.current.value;
+    const tags = tagsElement.current.value.split(" ");
+    userIdElement.current.value = "";
+    postTitleElement.current.value = "";
+    postBodyElement.current.value = "";
+    reactionsElement.current.value = "";
+    tagsElement.current.value = "";
+    addPost(userId, postTitle, postBody, reactions, tags);
+  };
   return (
-    <form className="text-white">
+    <form className="text-white create-post" onSubmit={handleSubmit}>
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
+        <label htmlFor="userId" className="form-label">
+          Enter Your User Id
         </label>
         <input
-          type="email"
+          ref={userIdElement}
+          type="text"
           className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
+          id="userId"
+          placeholder="Your User ID"
         />
-        <div id="emailHelp" className="form-text text-white">
-          We'll never share your email with anyone else.
-        </div>
       </div>
+
       <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          Password
+        <label htmlFor="title " className="form-label">
+          Post Title
         </label>
         <input
-          type="password"
+          ref={postTitleElement}
+          type="text"
           className="form-control"
-          id="exampleInputPassword1"
+          id="title"
+          placeholder="how are you feeling today..."
         />
       </div>
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" htmlFor="exampleCheck1">
-          Check me out
+
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post Content
         </label>
+        <textarea
+          ref={postBodyElement}
+          rows={4}
+          type="text"
+          className="form-control"
+          id="body"
+          placeholder="tell us more about it "
+        />
       </div>
+
+      <div className="mb-3">
+        <label htmlFor="reaction" className="form-label">
+          Number of Reactions
+        </label>
+        <input
+          ref={reactionsElement}
+          type="text"
+          className="form-control"
+          id="reaction"
+          placeholder="how people many reacted to this post. "
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="tag" className="form-label">
+          Enter Your Tags
+        </label>
+        <input
+          ref={tagsElement}
+          type="text"
+          className="form-control"
+          id="tag"
+          placeholder="how are you feeling today..."
+        />
+      </div>
+
       <button type="submit" className="btn btn-primary">
-        Submit
+        Post
       </button>
     </form>
   );
